8bcc3193767953e96366432efa8459e939fbe69a
Joe Klinck <jklinck76@gmail.com>
2026-02-17 18:35:35 -0800

    started problems 4 and 5

--- data_processing.py
+++ data_processing.py
@@ -1,6 +1,9 @@
+# This file is used by week_7
+import pandas as pd
+
+# Problem 1
 def rename_columns(df_raw):
     """
-    Problem #1
     Convert blank spaces to _
     Convert everything into lower case
     Remove trailing and leading spaces
@@ -9,46 +12,58 @@ def rename_columns(df_raw):
     I noticed you need to use strip() before replace otherwise it would replace any 
     leading or trailing spaces with a _
     """
+    df.raw
     df_raw.columns = df_raw.columns.str.strip()
     df_raw.columns = df_raw.columns.str.replace(' ', '_')
     df_raw.columns = df_raw.columns.str.lower()
     return df_raw
 
-def remove_fully_null_columns_rows():
+# Problem #2
+def remove_fully_null_columns_rows(df_raw):
     """
-    Problem #2
     Remove rows that are completely null
     Remove columns that are completely null
     Return a new DataFrame object
     """
     df_raw = df_raw.dropna(how="all")
     df_raw = df_raw.dropna(axis="columns", how="all")
-    return 1
+    return df_raw
 
-def clean_and_fill_content_rating():
+# Problem #3
+def clean_and_fill_content_rating(df_raw):
     """
-    Problem #3
     Convert all NaN values in content_rating to "Unrated"
     Convert all "Not Rated" with "Unrated"
     Return a new DataFrame object
     """
-    return 1
+    df_raw.loc[df_raw["content_rating"].isnull(), "content_rating"] = "Unrated"
+    df_raw.loc[df_raw["content_rating"] == "Not Rated", "content_rating"] = "Unrated"
+    return df_raw
 
-def clean_release_year():
+# Problem #4
+def clean_release_year(df_raw):
     """
-    Problem #4
     Add new column "release_year_coerce", use pd.to_datetime with errors="coerce"
     Add new column "release_year_mixed", use pd.to_datetime with errors="coerce" and format="mixed"
     Return a new DataFrame object
     """
-    return 1
+    df_raw["release_year_coerce"] = pd.to_datetime(
+        df_raw["release_year"], 
+        erorrs="coerce")
+    
+    df_raw["release_year_mixed"] = pd.to_datetime(df_raw["release_year"], erorrs="coerce", format="mixed", dayfist=True)
+    return df_raw
 
-def clean_income():
+# Problem #5
+def clean_income(df_raw):
     """
-    Problem #5
     Remove $, commas and other issues and covert to an int
     Return a new DataFrame object
     """
-    return 1
+    df_raw["income"] = df_raw["income"].str.replace(f'[$]',regex=True)
+    df_raw["income"] = df_raw["income"].str.replace('o', '0', regex=False)
+    df_raw["income"] = df_raw["income"].str.replace('0', '0', reges=False)
+    df_raw["income"] = df_raw["income"].astype("Int64")
+    return df_raw
 
 
